a792fe92ccb26d024dbccb3d21dd65ddcb7af083
Jasper van Merle <jaspervmerle@gmail.com>
2020-05-03 03:02:44 +0200

    Update Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import org.gradle.internal.deployment.RunApplication
-import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.2.71"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+    kotlin("jvm") version "1.3.72"
 }
 
 group = "com.jaspervanmerle.cglocal"
@@ -19,33 +18,17 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
-    compile("io.github.microutils:kotlin-logging:1.6.10")
-    compile("ch.qos.logback:logback-classic:1.2.3")
-    compile("com.mihnita:color-loggers:1.0.5")
-    compile("no.tornado:tornadofx:1.7.17")
-    compile("de.jensd:fontawesomefx:8.9")
-    compile("org.java-websocket:Java-WebSocket:1.3.9")
-    compile("org.json:json:20180813")
-    compile("name.mitterdorfer.perlock:perlock-core:0.3.1")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.github.microutils:kotlin-logging:1.6.10")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.mihnita:color-loggers:1.0.5")
+    implementation("no.tornado:tornadofx:1.7.17")
+    implementation("de.jensd:fontawesomefx:8.9")
+    implementation("org.java-websocket:Java-WebSocket:1.4.1")
+    implementation("org.json:json:20190722")
+    implementation("name.mitterdorfer.perlock:perlock-core:0.3.1")
 }
 
-tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    withType<Jar> {
-        doFirst {
-            manifest {
-                attributes(mapOf(
-                    "Implementation-Title" to "CGLocal",
-                    "Implementation-Version" to version,
-                    "Main-Class" to application.mainClassName
-                ))
-            }
-
-            from(configurations.compile.map { if (it.isDirectory) it else zipTree(it) })
-        }
-    }
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
