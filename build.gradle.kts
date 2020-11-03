cd620bc81c0a8474b20133eb879d7e976ce197d5
Jasper van Merle <jaspervmerle@gmail.com>
2020-11-03 02:47:18 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.3.72"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    kotlin("jvm") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "com.jaspervanmerle.cglocal"
 version = "1.2.0"
 
 application {
-    mainClassName = "$group.MainKt"
+    mainClass.set("$group.MainKt")
 }
 
 repositories {
@@ -21,14 +21,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("io.github.microutils:kotlin-logging:1.6.10")
+    implementation("io.github.microutils:kotlin-logging:2.0.3")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.mihnita:color-loggers:1.0.5")
-    implementation("org.java-websocket:Java-WebSocket:1.4.1")
-    implementation("org.json:json:20190722")
+    implementation("org.java-websocket:Java-WebSocket:1.5.1")
+    implementation("org.json:json:20200518")
     implementation("name.mitterdorfer.perlock:perlock-core:0.3.1")
-    implementation("org.koin:koin-core:2.1.5")
-    implementation("com.github.jiconfont:jiconfont-swing:1.0.0")
+    implementation("org.koin:koin-core:2.1.6")
+    implementation("com.github.jiconfont:jiconfont-swing:1.0.1")
     implementation("com.github.jiconfont:jiconfont-font_awesome:4.7.0.1")
     implementation("com.miglayout:miglayout:3.7.4")
 }
