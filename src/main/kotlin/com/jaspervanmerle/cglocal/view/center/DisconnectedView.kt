45192678c5194a6f72491c4e573a9b8d69615df5
Jasper van Merle <jaspervmerle@gmail.com>
2021-08-27 09:47:01 +0200

    Fix Open CodinGame button on non-GNOME Linux

--- src/main/kotlin/com/jaspervanmerle/cglocal/view/center/DisconnectedView.kt
+++ src/main/kotlin/com/jaspervanmerle/cglocal/view/center/DisconnectedView.kt
@@ -20,11 +20,26 @@ class DisconnectedView : View("wrap 1, insets 20, align center", "align center")
                 addActionListener {
                     val url = "https://www.codingame.com/"
 
-                    // On Linux Desktop.getDesktop().browse() hangs the application
+                    // On Linux Desktop.getDesktop().browse() hangs the application if it's available
                     // This issue can be fixed by running the call in a separate thread
                     thread {
                         logger.info("Opening '$url'")
-                        Desktop.getDesktop().browse(URL(url).toURI())
+
+                        var urlOpened = false
+
+                        if (Desktop.isDesktopSupported()) {
+                            val desktop = Desktop.getDesktop()
+                            if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                                desktop.browse(URL(url).toURI())
+                                urlOpened = true
+                            }
+                        }
+
+                        // On Linux the BROWSE action is only supported if the GNOME libraries are available
+                        // We try to use xdg-open if that's not the case
+                        if (!urlOpened) {
+                            Runtime.getRuntime().exec("xdg-open $url")
+                        }
                     }
                 }
             }
